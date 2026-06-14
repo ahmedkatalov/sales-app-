72dd0197cbe7d20761ef3ac8cf17c8e0e21582c5
Akhmed <aaaakk122123@gmail.com>
2026-06-14 20:40:41 +0300

    j

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
- 
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -11,6 +11,9 @@ export default defineConfig({
     allowedHosts: ['okvionsales.ru', 'www.okvionsales.ru'],
     host: '0.0.0.0',
     port: 5173,
+    watch: {
+      ignored: ['**/Dockerfile', '**/*.conf', '**/go.*', '**/*.go'],
+    },
     proxy: {
       '/api': {
         target: 'http://backend:3000',
